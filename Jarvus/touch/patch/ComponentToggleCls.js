b697788995fa5bb6b9cc66b14650ec09c32abce8
Chris Alfano <themightychris@gmail.com>
2013-07-24 18:17:29 -0400

    removed tabs

--- Jarvus/touch/patch/ComponentToggleCls.js
+++ Jarvus/touch/patch/ComponentToggleCls.js
@@ -3,10 +3,10 @@
  * Bug report: http://www.sencha.com/forum/showthread.php?267741-2.2.1-Component.toggleCls-does-not-update-internal-cls-config
  */
 Ext.define('Jarvus.touch.patch.ComponentToggleCls', {
-  override: 'Ext.Component',
+    override: 'Ext.Component',
 
-	toggleCls: function(className, force) {
-		var oldCls = this.getCls();
-		this[!force && oldCls && Ext.Array.contains(oldCls, className) ? 'removeCls' : 'addCls'](className);
-	}
+    toggleCls: function(className, force) {
+        var oldCls = this.getCls();
+        this[!force && oldCls && Ext.Array.contains(oldCls, className) ? 'removeCls' : 'addCls'](className);
+    }
 });
